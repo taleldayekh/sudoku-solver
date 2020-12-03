adf60dab2b63343baeb64fa9abb8d1c00cb7a545
jimogren <jim.ogren@gmail.com>
2020-12-03 08:17:34 +0100

    Added if __name__ == __main__

--- server/sudoku.py
+++ server/sudoku.py
@@ -5,26 +5,25 @@ NROWS = 9
 NCOLS = 9
 
 
-
-def writeSudokuToFile(board: List[int], filename: str) -> None:
+def write_to_file(board: List[int], filename: str) -> None:
     with open(filename, "w") as outfile:
         json.dump(board, outfile)
 
 
-def readSudokuFromFile(filename: str) -> List[int]:
+def read_from_file(filename: str) -> List[int]:
     with open(filename, "r") as infile:
         return json.load(infile)
 
 
-def printSudoku_simple(board: List[int]) -> None:
+def print_sudoku_simple(board: List[int]) -> None:
     for row in range(NROWS):
         for col in range(NCOLS):
             num = board[row * NROWS + col]
             print(num, end=" ")
-        print(f"\n", end="")
+        print(" ")
 
 
-def printSudoku(board: List[int]) -> None:
+def print_sudoku(board: List[int]) -> None:
     divider = "|———————|———————|———————|"
     for row in range(NROWS):
         if row % 3 == 0:
@@ -40,23 +39,101 @@ def printSudoku(board: List[int]) -> None:
         print("|")
     print(divider)
 
-# 0-8 is row 0 , 9-17 row 1 etc.
-def getRowNumber(index: int) -> int:
-    return index//NROWS
 
-# [0,9,18,27,36,45,54,63,72] is column 0 etc.
-def getColumnNumber(index: int) -> int:
-    return index%NCOLS
+# 0-8 is row 0 , 9-17 row 1 etc.
+def get_row_number(index: int) -> int:
+    return index // NROWS
+
+
+# 0,9,18,27,36,45,54,63,72 is column 0 etc.
+def get_column_number(index: int) -> int:
+    return index % NCOLS
+
 
 # rows 0-2 and column 0-2 is box 1, rows 0-2 and column 3-5 is box 2 etc.
-def getBoxNumber(index: int) -> int:
-    return 3*(getRowNumber(index)//3) + getColumnNumber(index)//3
+def get_box_number(index: int) -> int:
+    return 3 * (get_row_number(index) // 3) + get_column_number(index) // 3
 
-index_board = [i for i in range(81)]
-printSudoku(index_board)
 
-for i in index_board:
-    r = getRowNumber(i)
-    c = getColumnNumber(i)
-    b = getBoxNumber(i)
-    print(f"{i}: {r}; {c}; {b}")
+def get_row(board: List[int], row: int) -> List[int]:
+    return [board[row * NROWS + i] for i in range(NCOLS)]
+
+
+def get_column(board: List[int], column: int) -> List[int]:
+    return [board[column + NROWS * i] for i in range(NROWS)]
+
+
+def get_box(board: List[int], box: int) -> List[int]:
+    boxlist = []
+    for i in range(NROWS * NCOLS):
+        if get_box_number(i) == box:
+            boxlist.append(board[i])
+    return boxlist
+
+
+def validate_region(region: List[int]) -> bool:
+    numbers_unique = set()
+    for num in region:
+        if num > 0:
+            if num in numbers_unique:
+                return False
+            numbers_unique.add(num)
+    return True
+
+
+def validate_row(board: List[int], row_index: int) -> bool:
+    row = get_row(board, row_index)
+    return validate_region(row)
+
+
+def validate_column(board: List[int], column_index: int) -> bool:
+    column = get_column(board, column_index)
+    return validate_region(column)
+
+
+def validate_box(board: List[int], box_index: int) -> bool:
+    box = get_box(board, box_index)
+    return validate_region(box)
+
+
+def validate_list_enries(board: List[int]) -> bool:
+    # Check size
+    if len(board) != NROWS * NCOLS:
+        return False
+    # check all ints
+    for num in board:
+        if isinstance(num, int):
+            continue
+        return False
+    # check number range
+    if max(board) > 9 or min(board) < 0:
+        return False
+    return True
+
+
+def validate_sudoku(board: List[int]) -> bool:
+    if not validate_list_enries(board):
+        return False
+    # validate all regions
+    for i in range(NROWS):
+        if not validate_row(board, i):
+            return False
+        if not validate_column(board, i):
+            return False
+        if not validate_box(board, i):
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    S = read_from_file("example_sudoku.json")
+    print_sudoku(S)
+    print(validate_sudoku(S))
+    S[0] = 1
+    S[1] = 1
+    print_sudoku(S)
+    print(validate_sudoku(S))
+
+    S_solved = read_from_file("example_sudoku_solved.json")
+    print_sudoku(S_solved)
+    print(validate_sudoku(S_solved))
