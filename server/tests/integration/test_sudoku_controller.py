17b1fd143c4ccdbed358ae8fbe14a1bf9cd806a7
jimogren <jim.ogren@gmail.com>
2020-12-23 16:28:26 +0100

    Added tests for hint api endpoint

--- server/tests/integration/test_sudoku_controller.py
+++ server/tests/integration/test_sudoku_controller.py
@@ -14,11 +14,12 @@ from server.tests.utils.mock_data import (
 
 REQUEST_HEADERS = {"Content-Type": "application/json"}
 SOLVE_ENDPOINT = "api/v1/sudoku/solve"
+HINT_ENDPOINT = "api/v1/sudoku/hint"
 
 
 @pytest.mark.usefixtures("api_server")
 class TestSudokuPOST:
-    def test_solvable_sudoku_returns_solved_sudoku(self) -> None:
+    def test_solve_solvable_sudoku_returns_solved_sudoku(self) -> None:
         res = self.api.post(
             SOLVE_ENDPOINT,
             data=json.dumps({"sudoku": VALID_SUDOKU}),
@@ -29,7 +30,7 @@ class TestSudokuPOST:
         assert res_data["data"] == VALID_SUDOKU_SOLVED
         assert res.status_code == 200
 
-    def test_unsolvable_sudoku_returns_200(self) -> None:
+    def test_solve_unsolvable_sudoku_returns_200(self) -> None:
         res = self.api.post(
             SOLVE_ENDPOINT,
             data=json.dumps({"sudoku": UNSOLVABLE_SUDOKU}),
@@ -40,7 +41,7 @@ class TestSudokuPOST:
         assert res_data["data"] == "Sudoku is unsolvable"
         assert res.status_code == 200
 
-    def test_invalid_json_key_returns_400(self) -> None:
+    def test_solve_invalid_json_key_returns_400(self) -> None:
         res = self.api.post(
             SOLVE_ENDPOINT,
             data=json.dumps({"not-a-valid-key": VALID_SUDOKU}),
@@ -51,9 +52,53 @@ class TestSudokuPOST:
         assert res_data["error"] == "Invalid JSON key"
         assert res.status_code == 400
 
-    def test_invalid_sudoku_returns_400(self) -> None:
+    def test_solve_invalid_sudoku_returns_400(self) -> None:
         res = self.api.post(
             SOLVE_ENDPOINT,
+            data=json.dumps({"sudoku": INVALID_SUDOKU}),
+            headers=REQUEST_HEADERS,
+        )
+        res_data = json.loads(res.get_data(as_text=True))
+
+        assert res_data["error"] == "Not a valid sudoku"
+        assert res.status_code == 400
+
+    def test_hint_solvable_sudoku_returns_correct_hint(self) -> None:
+        res = self.api.post(
+            HINT_ENDPOINT,
+            data=json.dumps({"sudoku": VALID_SUDOKU}),
+            headers=REQUEST_HEADERS,
+        )
+        res_data = json.loads(res.get_data(as_text=True))
+
+        assert VALID_SUDOKU_SOLVED[res_data["data"][0]] == res_data["data"][1]
+        assert res.status_code == 200
+
+    def test_hint_unsolvable_sudoku_returns_200(self) -> None:
+        res = self.api.post(
+            HINT_ENDPOINT,
+            data=json.dumps({"sudoku": UNSOLVABLE_SUDOKU}),
+            headers=REQUEST_HEADERS,
+        )
+        res_data = json.loads(res.get_data(as_text=True))
+
+        assert res_data["data"] == "Sudoku is unsolvable"
+        assert res.status_code == 200
+
+    def test_hint_invalid_json_key_returns_400(self) -> None:
+        res = self.api.post(
+            HINT_ENDPOINT,
+            data=json.dumps({"not-a-valid-key": VALID_SUDOKU}),
+            headers=REQUEST_HEADERS,
+        )
+        res_data = json.loads(res.get_data(as_text=True))
+
+        assert res_data["error"] == "Invalid JSON key"
+        assert res.status_code == 400
+
+    def test_hint_invalid_sudoku_returns_400(self) -> None:
+        res = self.api.post(
+            HINT_ENDPOINT,
             data=json.dumps({"sudoku": INVALID_SUDOKU}),
             headers=REQUEST_HEADERS,
         )
